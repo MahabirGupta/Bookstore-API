a788ac6c70e4b0dc8673077fe9098d2ac9f1d164
MahabirGupta <mahabirg81@gmail.com>
2026-04-17 20:26:37 +0800

    Create DuplicateResourceException, InvalidRequestException and ResourceNotFoundException classes

---
+++ src/main/java/com/example/bookstore_api/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package com.example.bookstore_api.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
